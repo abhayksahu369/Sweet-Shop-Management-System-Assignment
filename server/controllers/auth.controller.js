d52a1af02152cec02eabf30a739082a0de995cad
Abhay Kumar Sahu <abhayksahu.cs@gmail.com>
2025-11-16 21:11:16 +0530

    small change

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -57,7 +57,7 @@ const login = async (req, res) => {
         id: user._id,
         name: user.name,
         email: user.email,
-        role: user.role,
+        isAdmin: user.isAdmin,
       },
     });
   } catch (err) {
